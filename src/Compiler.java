a0fe55a801d969ee611663d80625cfd7142a63cd
Modem514 <indigea514@gmail.com>
2022-10-29 10:29:52 +0800

    fix: complier api adjustment

--- src/Compiler.java
+++ src/Compiler.java
@@ -2,6 +2,6 @@ import org.kadf.app.eureka.*;
 
 public class Compiler {
     public static void main(String[] args) {
-        EurekacKt.eureka(System.in);
+        new EurekaCompiler(System.in).eureka();
     }
 }
