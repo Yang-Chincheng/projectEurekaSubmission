b79a066519679d1d83947d847e19edb6564fcc77
Modem514 <indigea514@gmail.com>
2023-02-02 08:24:16 +0800

    feat: codegen with graph coloring register allocation strategy v1.2

--- src/Compiler.java
+++ src/Compiler.java
@@ -2,6 +2,6 @@ import org.kadf.app.eureka.*;
 
 public class Compiler {
     public static void main(String[] args) {
-        new EurekaCompiler().eureka(System.in);
+        new EurekaCompiler().eureka(System.in, null, null);
     }
 }
