d0449955a9f004665b319596f0a67ac4ee90f2f7
Modem514 <indigea514@gmail.com>
2023-02-04 02:13:25 +0800

    feat: mem2reg & block fusion & local cse

--- src/Compiler.java
+++ src/Compiler.java
@@ -2,6 +2,6 @@ import org.kadf.app.eureka.*;
 
 public class Compiler {
     public static void main(String[] args) {
-        new EurekaCompiler().eurekaOJ(System.in, null, null);
+        new EurekaCompiler().eurekaOJ(System.in);
     }
 }
